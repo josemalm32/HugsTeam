78182dbc8f56b962a2e3c0f50061fdf12161edc3
gem install sass <hsanchezs01ex@gmail.com>
2017-02-13 13:34:31 +0100

    mew

---
+++ src/Constructor.java
@@ -0,0 +1,28 @@
+
+
+public class Constructor {
+
+	public boolean hayZetas(CharSequence cadena) {
+		for (int i = 0; i < cadena.length(); i++) {
+			if (Character.toLowerCase(cadena.charAt(i)) == 'z')
+				return true;
+		}
+		return false;
+	}
+
+	public static void main(String[] args) {
+
+		StringBuffer uno = new StringBuffer();
+		StringBuffer dos = new StringBuffer(80);
+		StringBuffer tres = new StringBuffer("Godofredo");
+		Constructor instancia = new Constructor();
+
+		System.out.println("longitudes " + uno.length() + " " + dos.length() + " " + tres.length());
+		System.out.println("capacidades " + uno.capacity() + " " + dos.capacity() + " " + tres.capacity());
+		
+		String cosa="si hay z";
+		StringBuilder cosaBuilder = new StringBuilder("hay zeta");
+		StringBuffer cosaBuffer = new StringBuffer("no hay zeta");
+		System.out.println("hay zetas "+instancia.hayZetas(cosaBuilder) );
+	}
+}
