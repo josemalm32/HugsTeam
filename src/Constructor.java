b6e9064d7a43013102d462e1a456c667665d32f1
josemalm32 <josema.aa14@gmail.com>
2017-02-14 09:04:59 +0100

    Nuevas Clases

--- src/Constructor.java
+++ src/Constructor.java
@@ -1,4 +1,4 @@
-
+import java.util.StringTokenizer;
 
 public class Constructor {
 
@@ -12,21 +12,33 @@ public class Constructor {
 	}
 
 	
-	
-	
-	public static void main(String[] args) {
-
-		StringBuffer uno = new StringBuffer();
-		StringBuffer dos = new StringBuffer(80);
-		StringBuffer tres = new StringBuffer("Godofredo");
-		Constructor instancia = new Constructor();
-
-		System.out.println("longitudes " + uno.length() + " " + dos.length() + " " + tres.length());
-		System.out.println("capacidades " + uno.capacity() + " " + dos.capacity() + " " + tres.capacity());
-		
-		String cosa="si hay z";
-		StringBuilder cosaBuilder = new StringBuilder("hay zeta");
-		StringBuffer cosaBuffer = new StringBuffer("no hay zeta");
-		System.out.println("hay zetas "+instancia.hayZetas(cosaBuilder) );
-	}
 }
+	
+//	public static void main(String[] args) {
+//
+//		StringBuffer uno = new StringBuffer();
+//		StringBuffer dos = new StringBuffer(80);
+//		StringBuffer tres = new StringBuffer("Godofredo");
+//		Constructor instancia = new Constructor();
+//
+//		System.out.println("longitudes " + uno.length() + " " + dos.length() + " " + tres.length());
+//		System.out.println("capacidades " + uno.capacity() + " " + dos.capacity() + " " + tres.capacity());
+//
+//		String cosa = "si hay z";
+//		StringBuilder cosaBuilder = new StringBuilder("hay zeta");
+//		StringBuffer cosaBuffer = new StringBuffer("no hay zeta");
+//		System.out.println("hay zetas " + instancia.hayZetas(cosaBuilder));
+//		// Tokenizer
+//		String nombre = "Angel;Franco;Garcia";
+//		StringTokenizer tokens = new StringTokenizer(nombre, ";");
+//		while (tokens.hasMoreTokens()) {
+//			System.out.println(tokens.nextElement());
+//		}
+//
+//		String[] lsitaNombres = nombre.split(";");
+//		for (int i = 0; i < lsitaNombres.length; i++) {
+//			System.out.println(lsitaNombres[i]);
+//		}
+//
+//	}
+//}
---
+++ src/Tokenizer.java
@@ -0,0 +1,25 @@
+import java.util.StringTokenizer;
+
+public class Tokenizer {
+
+	
+	public static void main(String[] args) {
+		
+		Tokenizer instancia  = new Tokenizer();
+
+	}
+	
+	
+	public Tokenizer() {
+		
+		String nombres = Texto.dameTexto();
+		StringTokenizer tokenizer = new StringTokenizer(nombres,",");
+		while (tokenizer.hasMoreElements()) {
+			
+			if (((String) tokenizer.nextElement()).charAt(0) != ' ')
+					System.out.println("Coma Encotrada");
+			
+		}
+	}
+	
+}
